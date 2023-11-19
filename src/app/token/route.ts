6c24182a215c1696005dee9c4802df5fb3fbdd0a
Lucas Tempass <lucas.tempass@hotmail.com>
2023-11-19 16:38:17 -0300

    Ajusta endpoint para receber token

--- src/app/token/route.ts
+++ src/app/token/route.ts
@@ -1,12 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
-import { kv } from "@vercel/kv";
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
+  const { code } = await req.json();
 
-  console.log(body);
+  const response = await fetch(process.env.NEXT_PUBLIC_TOKEN_URL as string, {
+    method: "POST",
+    body: JSON.stringify({
+      grantType: "authorization_code",
+      authorizationCode: code,
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+    }),
+  });
 
-  await kv.set("client:token", body);
+  if (response.status !== 200) {
+    return NextResponse.json(
+      {
+        error: "invalid_grant",
+        error_description: "Invalid authorization code.",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  const data = await response.json();
+
+  console.log("data", data);
 
   return NextResponse.json({
     message: "ok",
