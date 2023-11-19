5ba9339c187762ee5f957c778eeec50d1c250aaf
Lucas Tempass <lucas.tempass@hotmail.com>
2023-11-19 16:08:29 -0300

    Adiciona kv

--- src/app/token/route.ts
+++ src/app/token/route.ts
@@ -1,9 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
+import { kv } from "@vercel/kv";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
 
   console.log(body);
+
+  await kv.set("client:token", body);
 
   return NextResponse.json({
     message: "ok",
